c2c6ebc38f51552529599d828daf949d9d206cb1
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-05-21 04:08:07 +0000

    Feat: add supplement options for cascading dropdown

---
+++ assets/js/cascading-dropdown.js
@@ -0,0 +1,15 @@
+// List of supplements for each type
+const supplements = {
+    Nootropic: [
+        { name: 'Nootropic 1', value: 'nootropic1-content' },
+        { name: 'Nootropic 2', value: 'nootropic2-content' },
+    ],
+    Mineral: [
+        { name: 'Mineral 1', value: 'mineral1-content' },
+        { name: 'Mineral 2', value: 'mineral2-content' },
+    ],
+    Vitamin: [
+        { name: 'Vitamin 1', value: 'vitamin1-content' },
+        { name: 'Vitamin 2', value: 'vitamin2-content' },
+    ],
+};
