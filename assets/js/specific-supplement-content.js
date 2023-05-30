e512a5806188d58040e42ce81f94931a8d8ca03f
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-05-30 19:26:03 +0000

    Feat: populate area with default selection

--- assets/js/specific-supplement-content.js
+++ assets/js/specific-supplement-content.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const supplementDropdown = document.getElementById('supplement-type-dropdown');
     const supplementContentSections = Array.from(document.getElementsByClassName('supplement-content'));
 
-    // Function to display content based on information choice
+    // Function to display content based on supplement choice
     function showContent(supplement) {
         supplementContentSections.forEach(supplementContentSection => {
             if (supplementContentSection.id === supplement) {
@@ -16,4 +16,7 @@ document.addEventListener('DOMContentLoaded', function () {
     supplementDropdown.addEventListener('change', function () {
         showContent(supplementDropdown.value);
     });
+
+    // Display content based on initial selection
+    showContent(supplementDropdown.value);
 });
