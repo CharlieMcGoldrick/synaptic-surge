0ddf3e24fbf2f08e1a7deb5ed49c2375d0623e2d
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-05-30 23:54:20 +0000

    Fix: text resizing when menu opens/closes

--- assets/js/hamburger-sliding-side-panel.js
+++ assets/js/hamburger-sliding-side-panel.js
@@ -1,12 +1,15 @@
 const hamburgerIcon = document.getElementById('hamburger');
 const sidePanel = document.getElementById('mobile-nav');
 
+// Function to change visbility to visible if sidepanel is hidden, else set visible to hidden
 hamburgerIcon.addEventListener('click', function () {
     hamburgerIcon.classList.toggle('change');
 
-    if (sidePanel.style.width === "0px" || !sidePanel.style.width) {
-        sidePanel.style.width = "100%";
+    if (sidePanel.style.visibility === "hidden" || !sidePanel.style.visibility) {
+        sidePanel.style.visibility = "visible";
+        sidePanel.style.opacity = "1";
     } else {
-        sidePanel.style.width = "0px";
+        sidePanel.style.visibility = "hidden";
+        sidePanel.style.opacity = "0";
     }
 });
