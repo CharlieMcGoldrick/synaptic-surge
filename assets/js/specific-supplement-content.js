1b2bcc55768a8aa785b884755a270cdf93fc4c51
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-05-30 19:25:28 +0000

    Feat: add function to display content on info choice

--- assets/js/specific-supplement-content.js
+++ assets/js/specific-supplement-content.js
@@ -1,4 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
     const supplementDropdown = document.getElementById('supplement-type-dropdown');
     const supplementContentSections = Array.from(document.getElementsByClassName('supplement-content'));
+
+    // Function to display content based on information choice
+    function showContent(supplement) {
+        supplementContentSections.forEach(supplementContentSection => {
+            if (supplementContentSection.id === supplement) {
+                supplementContentSection.style.display = 'block';
+            } else {
+                supplementContentSection.style.display = 'none';
+            }
+        });
+    }
+
+    supplementDropdown.addEventListener('change', function () {
+        showContent(supplementDropdown.value);
+    });
 });
