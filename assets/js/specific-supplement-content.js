7d79aeab02b8bb9f99084ad25a792e46da19de91
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-05-30 19:24:19 +0000

    Feat: add specific-supplement-content.js with consts

---
+++ assets/js/specific-supplement-content.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const supplementDropdown = document.getElementById('supplement-type-dropdown');
+    const supplementContentSections = Array.from(document.getElementsByClassName('supplement-content'));
+});
