d6aa79a6fa2114251da16396d9ae2e5f8d886a2d
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-06-13 00:47:01 +0000

    Fix: close mobile menu when clicking on a link on the same page

--- assets/js/hamburger-sliding-side-panel.js
+++ assets/js/hamburger-sliding-side-panel.js
@@ -13,3 +13,16 @@ hamburgerIcon.addEventListener('click', function () {
         sidePanel.style.opacity = "0";
     }
 });
+
+// Select all links inside the mobile menu
+const mobileMenuLinks = document.querySelectorAll('#mobile-nav a');
+
+// Add a click event listener to each link
+for (let i = 0; i < mobileMenuLinks.length; i++) {
+    mobileMenuLinks[i].addEventListener('click', function () {
+        // Hide the mobile menu and toggle the hamburger icon
+        sidePanel.style.visibility = "hidden";
+        sidePanel.style.opacity = "0";
+        hamburgerIcon.classList.toggle('change');
+    });
+}
